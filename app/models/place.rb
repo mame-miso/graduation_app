01433d3b6d5bd487e3ca9a2bf3f650a0fb52b996
rq-mame-miso-at-321899771753 <mametamame@gmail.com>
2026-04-26 15:34:51 +0900

    未登録修正

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,15 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
-  has_one_attached :cover_image
+  has_one_attached :cover_image, dependent: :purge_later
+
+  validate :name_or_cover_image_present
+
+  private
+
+  def name_or_cover_image_present
+    if name.blank? && !cover_image.attached?
+      errors.add(:base, "場所名または写真のどちらかを登録してください")
+    end
+  end
 end
